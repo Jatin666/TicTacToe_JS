14c83ee3611139640d17ce4b2d714da24d17e3de
Jatin666 <verma.jatin6661@gmail.com>
2022-06-05 12:18:03 +0200

    Commit

---
+++ app.js
@@ -0,0 +1,124 @@
+
+const cases = [...document.getElementsByClassName("case")]; 
+let player = document.getElementById("player");
+let score1 = document.getElementById("score1");
+let score2 = document.getElementById("score2");
+let noresult = document.getElementById("noresult");
+
+let state = {
+  currentplayer: 1,
+  scoreJ1: 0,
+  scoreJ2: 0,
+  matchdraw: 0,
+  c1: 0,
+  c2: 0,
+  c3: 0,
+  c4: 0,
+  c5: 0,
+  c6: 0,
+  c7: 0,
+  c8: 0,
+  c9: 0,
+};
+
+
+
+
+const resetState = () => {
+  currentplayer = 1;
+  state.c1 = 0;
+  state.c2 = 0;
+  state.c3 = 0;
+  state.c4 = 0;
+  state.c5 = 0;
+  state.c6 = 0;
+  state.c7 = 0;
+  state.c8 = 0;
+  state.c9 = 0;
+};
+const winningpossibility = () => {
+//checking all winning possibilites
+  if (
+    (state.c1 == state.c2 && state.c2 == state.c3 && state.c1 > 0) ||
+    (state.c1 == state.c4 && state.c4 == state.c7 && state.c1 > 0) ||
+    (state.c1 == state.c5 && state.c5 == state.c9 && state.c1 > 0) ||
+    (state.c3 == state.c5 && state.c5 == state.c7 && state.c7 > 0) ||
+    (state.c2 == state.c5 && state.c5 == state.c8 && state.c2 > 0) ||
+    (state.c3 == state.c6 && state.c6 == state.c9 && state.c3 > 0) ||
+    (state.c4 == state.c5 && state.c5 == state.c6 && state.c4 > 0) ||
+    (state.c7 == state.c8 && state.c8 == state.c9 && state.c7 > 0)
+  ) {
+    console.log("winner !"); //if the the possibilities matches
+    return true;
+  } else if (
+    state.c1 !== 0 &&
+    state.c2 !== 0 &&
+    state.c3 !== 0 &&
+    state.c4 !== 0 &&
+    state.c5 !== 0 &&
+    state.c6 !== 0 &&
+    state.c7 !== 0 &&
+    state.c8 !== 0 &&
+    state.c9 !== 0
+  ) {
+    return null;
+  } else {
+    return false;
+  }
+};
+
+
+const playcase = (e) => {
+  let idCase = e.target.id;
+
+  if (state[idCase] !== 0) return;
+
+  state[idCase] = state.currentplayer;
+
+  let isVictory= winningpossibility();
+
+  if (isVictory=== true) {
+    
+
+    alert("The winner is player " + state.currentplayer);
+
+    if (state.currentplayer == 1) {
+      state.scoreJ1++;
+      score1.textContent = state.scoreJ1;
+    } else {
+      state.scoreJ2++;
+      score2.textContent = state.scoreJ2;
+    }
+
+    resetState();
+    cases.forEach((c) => (c.textContent = ""));
+  } else if (isVictory=== null) {
+    
+
+    alert("Match Draw !");
+
+    state.matchdraw++;
+    noresult.textContent = state.matchdraw;
+    player.textContent = "1";
+
+    resetState();
+    cases.forEach((c) => (c.textContent = ""));
+  } else if (isVictory=== false) {
+  
+    if (state.currentplayer == 1) {
+      state.currentplayer = 2;
+      e.target.textContent = "X";
+      player.textContent = "2";
+    } else {
+      state.currentplayer = 1;
+      e.target.textContent = "O";
+      player.textContent = "1";
+    }
+  }
+};
+
+cases.forEach((el) => {
+  el.addEventListener("click", playcase);
+});
+
+
